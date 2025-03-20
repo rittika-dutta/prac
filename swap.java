5510540fad6616f82a3062709e030c6e7d982fb9
UEM <rittika.1.dutta@gmail.com>
2025-03-20 15:18:06 +0530

    aaaaa

--- swap.java
+++ swap.java
@@ -2,13 +2,8 @@ class swap
 {
  public static void main(String args[])
 {
- int a=0,b=1,temp=0;
+ for(int i=1;i<=10;i++)
+ System.out.println(i+" ");
  
- temp=a;
- a=b;
- b=temp;
- 
- System.out.println("a = "+a);
- System.out.println("b = "+b);
 }
 }
